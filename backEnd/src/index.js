3117da8140b721de722db3e5dd5d4a2bd5b1383a
mBadar-dev <m.badar.dev@gmail.com>
2025-01-27 00:14:29 +0500

    update the index for appriopriate function

---
+++ backEnd/src/index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/database.js";
+import userRouter from "./routes/user.routes.js";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+connectDB(process.env.MONGO_URL);
+
+app.use((req, res, next)=>{
+  console.log(req.method, req.path);
+  next();
+})
+
+app.use("/user", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
