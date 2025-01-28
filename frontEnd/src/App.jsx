42904a0625f3edc9286ada5349e4a15b0f378a46
mBadar-dev <m.badar.dev@gmail.com>
2025-01-28 18:10:40 +0500

    add jsx in the app

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/signup";
 import Login from "./components/Login";
+import TodoList from "./components/todo";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<TodoList />} />
         </Routes>
       </Router>
     </>
